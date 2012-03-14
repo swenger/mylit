b7795e52964f4e4290cde14b7d490b4b566bb988
swenger <spwenger@googlemail.com>
2012-03-14 15:28:50 +0100

    increase version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name = "mylit",
-    version = "0.1.0",
+    version = "0.1.1",
     author = "Stephan Wenger",
     author_email = "wenger@cg.cs.tu-bs.de",
     description = "Literate programming for Python",
